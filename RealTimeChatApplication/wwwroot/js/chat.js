ced333fe2beb9d9caeb6d6f92f81abe9a42dcab8
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-08-30 23:23:42 +0300

    Implement chat js

---
+++ RealTimeChatApplication/wwwroot/js/chat.js
@@ -0,0 +1,30 @@
+﻿const connection = new signalR.HubConnectionBuilder().withUrl("/chat").build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    const msg = message
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;");
+
+    const encodedMsg = "[" + user + "]: " + msg;
+
+    const messageElement = document.createElement("h3");
+    messageElement.textContent = encodedMsg;
+
+    document.getElementId("messageList").appendChild(messageElement);
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    const user = document.getElementById("userInput").value;
+    const message = document.getElementById("messageInput").value;
+
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    event.preventDefault();
+});
