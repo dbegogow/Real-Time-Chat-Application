846c48f85941591a9d484ea519e8a0d59bef2152
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-08-31 23:38:13 +0300

    Remove privacy page

--- RealTimeChatApplication/Controllers/HomeController.cs
+++ RealTimeChatApplication/Controllers/HomeController.cs
@@ -12,11 +12,6 @@ namespace RealTimeChatApplication.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
