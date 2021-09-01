6d7b6f5e516533c7c39a09fa7ed99824b0e00f88
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-09-02 00:10:01 +0300

    Add send message component in chat component

--- client/real-time-chat-application/src/components/Chat.js
+++ client/real-time-chat-application/src/components/Chat.js
@@ -1,10 +1,12 @@
 import MessageContainer from "./MessageContainer";
+import SendMessageForm from "./SendMessageForm";
 
 const Chat = ({ messages, sendMessage }) => {
     return (
         <div>
             <div className="chat">
                 <MessageContainer messages={messages} />
+                <SendMessageForm sendMessage={sendMessage} />
             </div>
         </div>
     );
