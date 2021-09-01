708640cbcd2e70d1eea2955d829aadbe02dd4998
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-09-01 21:27:08 +0300

    Change project architecture

--- RealTimeChatApplication/Program.cs
+++ RealTimeChatApplication/Program.cs
@@ -12,7 +12,6 @@ namespace RealTimeChatApplication
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => webBuilder
-                    .UseStartup<Startup>());
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
--- RealTimeChatApplication/Startup.cs
+++ RealTimeChatApplication/Startup.cs
@@ -1,71 +1,33 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using RealTimeChatApplication.Data;
-using RealTimeChatApplication.Hubs;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace RealTimeChatApplication
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-            => this.Configuration = configuration;
-
-        public IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddDbContext<ApplicationDbContext>(options => options
-                .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services
-                .AddDefaultIdentity<IdentityUser>(options => options
-                    .SignIn
-                    .RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            services
-                .AddControllersWithViews();
-
-            services.AddSignalR();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app
-                    .UseDeveloperExceptionPage()
-                    .UseMigrationsEndPoint();
+                app.UseDeveloperExceptionPage();
             }
-            else
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
             {
-                app
-                    .UseExceptionHandler("/Home/Error")
-                    .UseHsts();
-            }
-
-            app
-                .UseHttpsRedirection()
-                .UseStaticFiles()
-                .UseRouting()
-                .UseAuthentication()
-                .UseAuthorization()
-                .UseEndpoints(endpoints =>
-                    {
-                        endpoints.MapDefaultControllerRoute();
-
-                        endpoints.MapRazorPages();
-
-                        endpoints.MapHub<ChatHub>("/chat");
-                    });
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
         }
     }
 }
