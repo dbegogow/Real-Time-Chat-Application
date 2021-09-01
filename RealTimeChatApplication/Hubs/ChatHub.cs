7956e08b4667b9fd139a9c4fe54d668e69c7d973
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-09-01 18:42:15 +0300

    Add logic in chat hub for sending messages only to one user

--- RealTimeChatApplication/Hubs/ChatHub.cs
+++ RealTimeChatApplication/Hubs/ChatHub.cs
@@ -1,11 +1,25 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Authorization;
 
 namespace RealTimeChatApplication.Hubs
 {
+    [Authorize]
     public class ChatHub : Hub
     {
+        public override Task OnConnectedAsync()
+        {
+            Groups.AddToGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
+            return base.OnConnectedAsync();
+        }
         public async Task SendMessage(string user, string message)
-            => await Clients.All.SendAsync("ReceiveMessage", user, message);
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public Task SendMessageToGroup(string sender, string receiver, string message)
+        {
+            return Clients.Group(receiver).SendAsync("ReceiveMessage", sender, message);
+        }
     }
 }
