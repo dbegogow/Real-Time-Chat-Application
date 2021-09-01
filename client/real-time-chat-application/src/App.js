193e446132569fa28ffb8f665895b399fc971adb
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-09-01 22:18:49 +0300

    Implement join room component

--- client/real-time-chat-application/src/App.js
+++ client/real-time-chat-application/src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Lobby from './components/Lobby';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="app">
+            <h2>MyChat</h2>
+            <hr className="line" />
+            <Lobby joinRoom={joinRoom} />
+        </div>
+    );
+};
 
 export default App;
